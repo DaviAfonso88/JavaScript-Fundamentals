be4e0f79174292dba022897159b08461a706c303
Davi Afonso <daviafonso5894@gmail.com>
2025-09-04 16:15:32 -0300

    Spread/Rest

--- esnext.js/operadorSpreadRest.js
+++ esnext.js/operadorSpreadRest.js
@@ -2,3 +2,11 @@
 // Usar rest com parâmetro de função
 
 // Usar spread com objeto
+const funcionario = { nome: "Maria", salario: 1234.99 };
+const clone = { ativo: true, ...funcionario };
+console.log(clone);
+
+// Usar spread com array
+const grupoA = ["João", "Pedro", "Gloria"];
+const grupoFinal = ["Maria", ...grupoA, "Rafaela"];
+console.log(grupoFinal);
